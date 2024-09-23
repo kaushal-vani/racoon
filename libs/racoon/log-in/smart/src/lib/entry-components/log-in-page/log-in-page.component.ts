f56a319a9dafb824416fb987e582fb2ad5bc6021
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 11:44:02 +0530

    create log-in page entry component

---
+++ libs/racoon/log-in/smart/src/lib/entry-components/log-in-page/log-in-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-log-in-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './log-in-page.component.html',
+  styleUrl: './log-in-page.component.scss'
+})
+export class LogInPageComponent {
+
+}
