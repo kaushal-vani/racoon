d6d344e1393152e26a7acb6cad64b65950e6c85e
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 18:36:25 +0530

    create ui log-in component

---
+++ libs/racoon/log-in/ui/src/lib/features/log-in/log-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-log-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './log-in.component.html',
+  styleUrl: './log-in.component.scss'
+})
+export class LogInComponent {
+
+}
