122c149e7dc46026ed19e02ea08a68c48370bbc3
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 11:59:41 +0530

    create payment page entry component

---
+++ libs/racoon/payment/smart/src/lib/entry-components/payment-page/payment-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-payment-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-page.component.html',
+  styleUrl: './payment-page.component.scss'
+})
+export class PaymentPageComponent {
+
+}
