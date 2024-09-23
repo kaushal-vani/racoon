8c5ab3ab859006c0d2d89caed716b944630e92fd
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 11:56:35 +0530

    create about-us page entry component

---
+++ libs/racoon/about-us/smart/src/lib/entry-components/about-us-page/about-us-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-about-us-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-us-page.component.html',
+  styleUrl: './about-us-page.component.scss'
+})
+export class AboutUsPageComponent {
+
+}
