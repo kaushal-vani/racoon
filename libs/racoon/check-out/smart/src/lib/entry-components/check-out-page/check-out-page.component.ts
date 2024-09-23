98b5ba663d8187274fde68973c23dcb29ddd6821
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 11:49:58 +0530

    create check-out page entry component

---
+++ libs/racoon/check-out/smart/src/lib/entry-components/check-out-page/check-out-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-check-out-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './check-out-page.component.html',
+  styleUrl: './check-out-page.component.scss'
+})
+export class CheckOutPageComponent {
+
+}
