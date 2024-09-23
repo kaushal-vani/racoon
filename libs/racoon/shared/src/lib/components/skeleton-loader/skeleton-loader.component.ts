d7e00ac999f4b96cff45b2b3b9d1704061c172ed
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 15:33:31 +0530

    create shared skeleton-loader component

---
+++ libs/racoon/shared/src/lib/components/skeleton-loader/skeleton-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-skeleton-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './skeleton-loader.component.html',
+  styleUrl: './skeleton-loader.component.scss'
+})
+export class SkeletonLoaderComponent {
+
+}
