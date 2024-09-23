bb95b0c083c4108a3cadd103576f9fd846cec2e1
kaushal2469 <kaushal2469@gmail.com>
2024-09-23 19:47:23 +0530

    add SCREEN_VIEW and BUTTON_NAME

---
+++ libs/racoon/shared/src/lib/enums/screen-view.enum.ts
@@ -0,0 +1,34 @@
+export const enum SCREEN_VIEW {
+    ADD_EMAIL = 'add-email',
+    ADD_PHONE_NUMBER = 'add-phone-number',
+    SELECT_DEVICE = 'select-device',
+    EDIT_EMAIL = 'edit-email',
+    EDIT_PHONE_NUMBER = 'edit-phone-number',
+    FORGOT_PASSWORD = 'forgot-password',
+    LOGIN = 'login',
+    ONE_TIME_PASSWORD = 'one-time-password',
+    SELECT_VERIFICATION_METHOD = 'select-verification_method',
+    SIGN_UP = 'sign-up',
+    VERIFICATION_COMPLETED = 'verification-completed',
+  }
+  
+  export const enum BUTTON_NAME {
+    CANCEL = 'cancel',
+    CONTINUE = 'continue',
+    EDIT = 'edit',
+    FORGOT_PASSWORD = 'forgot-password',
+    GO_TO_LOGIN = 'go-to-login',
+    GO_TO_SIGN_UP = 'go-to-sign-up',
+    LOGIN = 'login',
+    LOGOUT = 'logout',
+    NEXT = 'next',
+    OTP_SUBMIT = 'otp-submit',
+    PREVIOUS = 'previous',
+    RESEND = 'resend',
+    SAVE = 'save',
+    SIGN_UP = 'sign-up',
+    SUBMIT = 'submit',
+    VERIFY = 'verify',
+    SET_UP_LATER = 'set-up-later'
+  }
+  
