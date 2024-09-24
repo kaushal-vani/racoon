906fb39bc1cb74717ae2f383c1cd15967c57543d
kaushal2469 <kaushal2469@gmail.com>
2024-09-25 01:24:35 +0530

    rename authentication entry component

---
+++ libs/racoon/authentication/smart/src/lib/entry-components/authentication/authentication.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-authentication',
+  standalone: true,
+  imports: [],
+  templateUrl: './authentication.component.html',
+  styleUrl: './authentication.component.scss'
+})
+export class AuthenticationComponent {
+
+}
