b0feab25e604367309da35695e74bf57af4b674e
kaushal2469 <kaushal2469@gmail.com>
2024-09-25 01:21:16 +0530

    rename products entry component

--- libs/racoon/products/smart/src/lib/entry-components/products/products.component.spec.ts
+++ libs/racoon/products/smart/src/lib/entry-components/products/products.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductsPageComponent } from './products-page.component';
-
-describe('ProductsPageComponent', () => {
-  let component: ProductsPageComponent;
-  let fixture: ComponentFixture<ProductsPageComponent>;
+import { ProductsComponent } from './products.component'
+describe('ProductsComponent', () => {
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductsPageComponent]
+      imports: [ProductsComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ProductsPageComponent);
+    fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
