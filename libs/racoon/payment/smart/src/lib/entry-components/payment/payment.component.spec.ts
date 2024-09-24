049cfb4a71142b1dffb7bc2b5d0116218ceb8723
kaushal2469 <kaushal2469@gmail.com>
2024-09-25 01:16:03 +0530

    rename payment entry component

--- libs/racoon/payment/smart/src/lib/entry-components/payment/payment.component.spec.ts
+++ libs/racoon/payment/smart/src/lib/entry-components/payment/payment.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PaymentPageComponent } from './payment-page.component';
-
-describe('PaymentPageComponent', () => {
-  let component: PaymentPageComponent;
-  let fixture: ComponentFixture<PaymentPageComponent>;
+import { PaymentComponent } from './payment.component'
+describe('PaymentComponent', () => {
+  let component: PaymentComponent;
+  let fixture: ComponentFixture<PaymentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PaymentPageComponent]
+      imports: [PaymentComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PaymentPageComponent);
+    fixture = TestBed.createComponent(PaymentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ libs/racoon/payment/smart/src/lib/entry-components/payment/payment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-payment',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment.component.html',
+  styleUrl: './payment.component.scss'
+})
+export class PaymentComponent {
+
+}
