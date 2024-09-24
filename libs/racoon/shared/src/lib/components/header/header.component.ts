fb5420ac536be8faa2e1ba2d9fac353d3cfe5c9f
kaushal2469 <kaushal2469@gmail.com>
2024-09-25 00:28:55 +0530

    update header component

--- libs/racoon/shared/src/lib/components/header/header.component.ts
+++ libs/racoon/shared/src/lib/components/header/header.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  menuVisible = false;
+  userMenuVisible = false;
+  profileMenuVisible = false;
 
+  @ViewChild('profileMenu')
+  profileMenu!: ElementRef;
+  @ViewChild('profileMenuContent')
+  profileMenuContent!: ElementRef;
+
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  toggleUserMenu() {
+    this.userMenuVisible = !this.userMenuVisible;
+  }
+
+  toggleProfileMenu() {
+    this.profileMenuVisible = !this.profileMenuVisible;
+  }
+
+  closeProfileMenu() {
+    this.profileMenuVisible = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    if (!this.profileMenu.nativeElement.contains(event.target) && !this.profileMenuContent.nativeElement.contains(event.target)) {
+      this.profileMenuVisible = false;
+    }
+  }
 }
