3d09657fcc24e0e3113704f1ade26cc2a24da6c1
kaushal2469 <kaushal2469@gmail.com>
2024-09-25 07:06:02 +0530

    create select-device component

---
+++ libs/racoon/authentication/ui/src/lib/features/select-device/select-device.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-select-device',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './select-device.component.html',
+  styleUrl: './select-device.component.scss',
+})
+export class SelectDeviceComponent {}
